ebe0f973c63c7e9ddd5c25845e87fcc59215d6ab
ashleyang2001 <ashleyang2001@live.com>
2025-01-28 22:45:07 +0800

    Modified main method to echo commands entered by the user, and exits program when the user types the command "bye".

--- src/main/java/Prune.java
+++ src/main/java/Prune.java
@@ -1,7 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * The Prune class is a chatbot named Prune which interacts with the user to perform certain tasks.
+ */
 public class Prune {
+    /** Command to exit the program. **/
+    private static final String EXIT_COMMAND = "bye";
+
+    /**
+     * Main method to interact with the chatbot.
+     * The program first greets the user, and wait for user input.
+     * It echos commands entered by the user, and exits when the user types the command "bye".
+     * @param args Command line arguments, not used in program.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello! I am Prune.");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        // Exit command.
+        Scanner scanner = new Scanner(System.in);
+        // Greets user.
+        System.out.println("\tHello! I am Prune.");
+        String mascot = """
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⣶⣶⣶⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀
+                ⠀⠀⣠⣴⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣵⣄⠀⠀⠀
+                ⠀⢾⣻⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⡀⠀
+                ⠸⣽⣻⠃⣿⡿⠋⣉⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⡟⠉⡉⢻⣿⡌⣿⣳⡥⠀
+                ⢜⣳⡟⢸⣿⣷⣄⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣤⣠⣼⣿⣇⢸⢧⢣⠀
+                ⠨⢳⠇⣸⣿⣿⢿⣿⣿⣿⣿⡿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡟⢆⠀
+                ⠀⠀⠈⠀⣾⣿⣿⣼⣿⣿⣿⣿⡀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣽⣿⣿⠐⠈⠀⠀
+                ⢀⣀⣼⣷⣭⣛⣯⡝⠿⢿⣛⣋⣤⣤⣀⣉⣛⣻⡿⢟⣵⣟⣯⣶⣿⣄⡀⠀
+                ⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣾⣶⣶⣴⣾⣿⣿⣿⣿⣿⣿⢿⣿⣿⣧
+                ⣿⣿⣿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⣿⡿
+                """;
+        System.out.println(mascot);
+        System.out.println("\tWhat can I do for you?\n");
+        // Continuously ask for, and echos user's input until "bye" is inputted.
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals(EXIT_COMMAND)) {
+                // End the program when "bye" is inputted.
+                System.out.println("\tBye. Hope to see you again soon!\n");
+                break;
+            } else {
+                // Echos the input.
+                System.out.println("\t" + input + "\n");
+            }
+        }
+
     }
 }
